66cff1021fc0ac7778ad49bc5d1806657f55a7bb
yvam-augusto <yvam_augusto@hotmail.com>
2023-06-17 17:11:41 -0300

    feat: Adição de arquivo com as descrições que o sistema irá utilizar para auxiliar os usuários.

---
+++ rdo-diario-de-obra/frontend/src/js/fieldDescriptions.js
@@ -0,0 +1,25 @@
+//login
+loginDescriptionText = "Informe seu número de CPF e senha para acessar o sistema."
+
+//constructions
+constructionsDescriptionText = "Abaixo você pode visualizar uma lista de obras cadastradas no sistema. Para cada obra, é possível inserir novas informações diariamente referente ao andamento da obra."
+//constructionsDescription = "Aqui você pode visualizar as obras cadastradas no sistema. Para cada obra, é possível visualizar os dados cadastrados, bem como os relatórios de diário de obra gerados."
+
+//register
+//climate
+climateText = "Neste campo, informe o clima do dia para o período da manhã e tarde, sendo a primeira opção de céu aberto, a segunda nublado e a terceira chuvoso."
+conditionsText = "Neste campo, informe as condições do dia para o período da manhã e tarde, sendo a primeira opção praticável e a segunda impraticável."
+climateObservationText = "Neste campo, informe alguma observação referente ao clima do dia."
+//activities
+activitiesText = "Neste campo, informe as atividades realizadas no dia. É possível adicionar mais de uma atividade, clicando no botão 'Adicionar'."
+//occurrences
+ocurrencesText = "Neste campo, informe as ocorrências do dia. É possível adicionar mais de uma ocorrência, clicando no botão 'Adicionar'."
+//materialsUded
+materialsUsedText = "Neste campo, informe os materiais utilizados no dia. É possível adicionar mais de um material, clicando no botão 'Adicionar'."
+//workForce
+workForceText = "Neste campo, informe a equipe de trabalho do dia. É possível adicionar mais de uma equipe, clicando no botão 'Adicionar'."
+//attachments
+attachmentsText = "Neste campo, anexe arquivos de imagem referentes ao dia através do botão abaixo. É possível adicionar mais de um anexo, fazendo seleção de mais de uma imagem."
+attachmentsObservationText = "Neste campo, informe alguma observação referente aos anexos do dia."
+//comments
+commentText = "Neste campo, informe algum comentário adicional referente ao registro do dia."
